c13866f6a351013f77d4d6c9930a4a232cae687f
wthe <1390613445@qq.com>
2021-05-20 17:00:35 +0800

    add 3.枚举

---
+++ 3.拓展类型-枚举/src/index.ts
@@ -0,0 +1,19 @@
+enum Level {
+    level1 = 'a',
+    level2 = 'b',
+}
+
+enum Numbers {
+    num1,
+    num2
+}
+let n:Numbers = Numbers.num1;
+n = Numbers.num2;
+let l:Level = Level.level1;
+console.log(Level.level1);
+console.log('n',n);
+
+function getUsers(lev:Numbers){
+
+}
+getUsers(0);
